542ddd14df646b6879d945c6794c6395ec80c356
EbubeTheSage <38366016+EbubeTheSage@users.noreply.github.com>
2021-07-25 23:57:32 +0100

    Add project files.

---
+++ 27-06-21/Program.vb
@@ -0,0 +1,13 @@
+Imports System.Console
+Module Module1
+    Sub Main()
+        Write("King")
+        NewMethod()
+        Write("Francis")
+        ReadKey()
+    End Sub
+
+    Private Sub NewMethod()
+        Write(" ")
+    End Sub
+End Module
---
+++ HelloWorld/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("What is your name?")
+        Dim name = Console.ReadLine()
+        Console.WriteLine("Hello " + name + "!")
+    End Sub
+End Module
